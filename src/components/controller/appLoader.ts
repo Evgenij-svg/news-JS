94258d36551eb06d5cff0ec0313681b8a748bc46
Evgenij-svg <deshkevich.evgenij@gmail.com>
2023-06-07 16:10:47 +0300

    refactor:appLoader and loader

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,9 +1,9 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
+    private constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd89d3f1fcf374dd7a1f4a28d6c2d5fd3', // получите свой ключ https://newsapi.org/
         });
     }
 }
