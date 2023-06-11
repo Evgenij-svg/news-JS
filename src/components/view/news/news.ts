a5420306e16ed41d362319cd531ccccf5608506e
Evgenij-svg <deshkevich.evgenij@gmail.com>
2023-06-11 17:27:46 +0300

    refactor:source.ts

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -15,11 +15,8 @@ interface New {
 }
 
 class News {
-    protected draw(data: New[]): void {
-        console.log(data);
-        console.log('h1');
+    public draw(data: New[]): void {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
-        console.log(news);
         const fragment = document.createDocumentFragment();
         const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
 
---
+++ src/components/view/sources/sources.ts
@@ -0,0 +1,27 @@
+import './sources.css';
+
+interface New {
+    id: string;
+    name: string;
+}
+
+class Sources {
+    public draw(data: New[]): void {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+            const sourceItemName = sourceClone.querySelector('.source__item-name') as HTMLElement;
+            sourceItemName.textContent = item.name;
+            const sourceItemId = sourceClone.querySelector('.source__item') as HTMLElement;
+            sourceItemId.setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+        const sourceElement = document.querySelector('.sources') as HTMLElement;
+        sourceElement.append(fragment);
+    }
+}
+
+export default Sources;
